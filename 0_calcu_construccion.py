35cb31fdbe1e3b4850a21bf11adcb3db50ce9f1a
Luciano Nuñez <99107752+lucianonunnez@users.noreply.github.com>
2025-09-26 19:46:28 -0300

    Add files via upload

---
+++ 0_calcu_construccion.py
@@ -0,0 +1,23 @@
+from operaciones2 import costo_total,costo_aberturas
+
+puertas = 5
+ventanas = 8 
+madera = 100
+
+costo_puertas = costo_aberturas(puertas,madera)
+
+# operaciones2.py
+"""
+def costo_aberturas (aberturas,madera):
+    
+    total = aberturas*madera*1.6
+
+    return total
+
+def costo_total (puertas,ventanas,ambientes):
+    
+    total = puertas*ventanas*ambientes
+
+    return total
+
+"""
